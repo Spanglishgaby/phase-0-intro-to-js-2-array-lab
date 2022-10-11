88ac18b46d98084259342156b540f6fce2d91ba4
Yuriko <ygabriela.arnott@gmail.com>
2022-10-11 18:02:44 -0400

    commit tree 9fe9df9048e198deb9f56d233a9ed474e0a829e5

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+ function destructivelyAppendCat(name){
+   cats.push(name);
+ }
+ function destructivelyPrependCat(name){
+    cats.unshift(name);
+  }
+  function destructivelyRemoveLastCat(){
+    cats.pop();
+  }
+  function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+       
